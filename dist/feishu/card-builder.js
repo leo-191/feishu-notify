39bf0b04e9c939bf8de0483dd5f67a909f0ccc32
leo-191 <zeng.yuchen@hkaco.com>
2025-10-27 09:48:02 +0800

    build update version v1.1.2

--- dist/feishu/card-builder.js
+++ dist/feishu/card-builder.js
@@ -111,11 +111,11 @@ class CardBuilder {
         }
         else if (action === "review_requested") {
             iconToken = "member-new_outlined";
-            content = `${authorInfo} 请求对这项 Pull request 开展代码审查${branchInfo}`;
+            content = `${authorInfo} 请求对这项 Pull request 开展代码审查 ${branchInfo}`;
         }
         else {
             iconToken = "replace_outlined";
-            content = `${authorInfo} 更新了这项 Pull request`;
+            content = `${authorInfo} 更新了这项 Pull request ${branchInfo}`;
         }
         return {
             tag: "div",
