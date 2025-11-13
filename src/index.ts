24a1dd502ea4ebc446e3b31716800a0f075dad91
leo-191 <zeng.yuchen@hkaco.com>
2025-11-13 14:18:58 +0800

    refactor: divide data parsing into different func

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,78 @@ function getEventTypeFromArgs(): string {
   return eventType;
 }
 
+const parsePRInfo = (data: any): GithubPRInfo => {
+  const PRInfo: GithubPRInfo = {
+    action: data.action,
+    number: data.pull_request.number,
+    full_name: data.pull_request.base.repo.full_name,
+    title: data.pull_request.title,
+    body: data.pull_request.body || "",
+    html_url: data.pull_request.html_url,
+    sender: {
+      login: data.sender.login,
+      html_url: data.sender.html_url,
+    },
+    draft: data.pull_request.draft,
+    state: data.pull_request.state,
+    created_at: data.pull_request.created_at,
+    updated_at: data.pull_request.updated_at,
+    merged: data.pull_request.merged,
+    base: {
+      label: data.pull_request.base.label,
+    },
+    head: {
+      label: data.pull_request.head.label,
+    },
+  };
+  console.log("处理 PR 信息:", PRInfo);
+
+  return PRInfo;
+};
+
+const parseIssueInfo = (data: any): GithubIssueInfo => {
+  const issueInfo: GithubIssueInfo = {
+    action: data.action,
+    number: data.issue.number,
+    full_name: data.repository.full_name,
+    title: data.issue.title,
+    body: data.issue.body || "",
+    html_url: data.issue.html_url,
+    sender: {
+      login: data.sender.login,
+      html_url: data.sender.html_url,
+    },
+    state: data.issue.state,
+    created_at: data.issue.created_at,
+    updated_at: data.issue.updated_at,
+  };
+
+  console.log("处理 Issue 信息:", issueInfo);
+
+  return issueInfo;
+};
+
+const parseCommentInfo = (data: any): GitHubCommentInfo => {
+  const commentInfo: GitHubCommentInfo = {
+    action: data.action,
+    number: data.issue.number,
+    issue_type: data.issue.pull_request ? "pull_request" : "issues",
+    full_name: data.repository.full_name,
+    title: data.issue.title,
+    body: data.comment.body || "",
+    html_url: data.comment.html_url,
+    sender: {
+      login: data.sender.login,
+      html_url: data.sender.html_url,
+    },
+    created_at: data.comment.created_at,
+    updated_at: data.comment.updated_at,
+  };
+
+  console.log("处理 Issue Comment 信息:", commentInfo);
+  return commentInfo;
+};
+
 async function run() {
   try {
     const eventType = getEventTypeFromArgs();
@@ -40,79 +112,26 @@ async function run() {
     const eventData = JSON.parse(fs.readFileSync(eventPath, "utf8"));
 
     let card;
-    if (eventType === "pull_request") {
-      const PRInfo: GithubPRInfo = {
-        action: eventData.action,
-        number: eventData.pull_request.number,
-        full_name: eventData.pull_request.base.repo.full_name,
-        title: eventData.pull_request.title,
-        body: eventData.pull_request.body || "",
-        html_url: eventData.pull_request.html_url,
-        sender: {
-          login: eventData.sender.login,
-          html_url: eventData.sender.html_url,
-        },
-        draft: eventData.pull_request.draft,
-        state: eventData.pull_request.state,
-        created_at: eventData.pull_request.created_at,
-        updated_at: eventData.pull_request.updated_at,
-        merged: eventData.pull_request.merged,
-        base: {
-          label: eventData.pull_request.base.label,
-        },
-        head: {
-          label: eventData.pull_request.head.label,
-        },
-      };
 
-      console.log("处理 PR 信息:", PRInfo);
-
-      const cardBuilder = new CardBuilder();
-      card = cardBuilder.buildPRCard(PRInfo);
-    } else if (eventType === "issues") {
-      const issueInfo: GithubIssueInfo = {
-        action: eventData.action,
-        number: eventData.issue.number,
-        full_name: eventData.repository.full_name,
-        title: eventData.issue.title,
-        body: eventData.issue.body || "",
-        html_url: eventData.issue.html_url,
-        sender: {
-          login: eventData.sender.login,
-          html_url: eventData.sender.html_url,
-        },
-        state: eventData.issue.state,
-        created_at: eventData.issue.created_at,
-        updated_at: eventData.issue.updated_at,
-      };
-
-      console.log("处理 Issue 信息:", issueInfo);
-
-      const cardBuilder = new CardBuilder();
-      card = cardBuilder.buildIssueCard(issueInfo);
-    } else if (eventType === "issue_comment") {
-      const commentInfo: GitHubCommentInfo = {
-        action: eventData.action,
-        number: eventData.issue.number,
-        issue_type: eventData.issue.pull_request ? "pull_request" : "issues",
-        full_name: eventData.repository.full_name,
-        title: eventData.issue.title,
-        body: eventData.comment.body || "",
-        html_url: eventData.comment.html_url,
-        sender: {
-          login: eventData.sender.login,
-          html_url: eventData.sender.html_url,
-        },
-        created_at: eventData.comment.created_at,
-        updated_at: eventData.comment.updated_at,
-      };
-
-      console.log("处理 Issue Comment 信息:", commentInfo);
-
-      const cardBuilder = new CardBuilder();
-      card = cardBuilder.buildCommentCard(commentInfo);
-    } else {
-      throw new Error(`未知的事件类型: ${eventType}`);
+    switch (eventType) {
+      case "pull_request": {
+        const cardBuilder = new CardBuilder();
+        card = cardBuilder.buildPRCard(parsePRInfo(eventData));
+        break;
+      }
+      case "issues": {
+        const cardBuilder = new CardBuilder();
+        card = cardBuilder.buildIssueCard(parseIssueInfo(eventData));
+        break;
+      }
+      case "issue_comment": {
+        const cardBuilder = new CardBuilder();
+        card = cardBuilder.buildCommentCard(parseCommentInfo(eventData));
+        break;
+      }
+      default:
+        throw new Error(`未知的事件类型: ${eventType}`);
+        break;
     }
 
     const botClient = new FeishuBotClient(webhookUrl);
